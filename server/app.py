286aa3ea96c238015a99bce69abd97bb365d5ba5
john makateto <jmakateto@gmail.com>
2023-09-25 22:34:56 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,21 +1,51 @@
-
-
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-from models import db, User, Review, Game
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+db = SQLAlchemy()
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = 'games'
+    serialize_rules = ('-reviews.game',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    genre = db.Column(db.String)
+    platform = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    reviews = db.relationship('Review', backref='game')
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    serialize_rules = ('-game.reviews', '-user.reviews',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    serialize_rules = ('-reviews.user',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    reviews = db.relationship('Review', backref='user')
 
 @app.route('/')
 def index():
@@ -23,37 +53,17 @@ def index():
 
 @app.route('/games')
 def games():
-
-    games = []
-    for game in Game.query.all():
-        game_dict = {
-            "title": game.title,
-            "genre": game.genre,
-            "platform": game.platform,
-            "price": game.price,
-        }
-        games.append(game_dict)
-
-    response = make_response(
-        jsonify(games),
-        200
-    )
+    games = [game.to_dict() for game in Game.query.all()]
+    response = make_response(jsonify(games), 200)
     response.headers["Content-Type"] = "application/json"
-
     return response
 
 @app.route('/games/<int:id>')
 def game_by_id(id):
     game = Game.query.filter_by(id=id).first()
-
     game_dict = game.to_dict()
-
-    response = make_response(
-        jsonify(game_dict),
-        200
-    )
+    response = make_response(jsonify(game_dict), 200)
     response.headers["Content-Type"] = "application/json"
-
     return response
 
 if __name__ == '__main__':
